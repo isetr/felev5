c20ee5e9e03d1b419929489dbc781eeb65590656
Szalóki Sándor <setr@INF.ELTE.HU>
2017-10-19 09:09:37 +0200

    Opre | Gyak 05

---
+++ opre/gyak05/signal.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+
+void handler(int signumber){
+  printf("Signal with number %i has arrived\n",signumber);
+}
+
+int main(){
+
+    
+  signal(SIGRTMIN,handler); //handler = SIG_IGN - ignore the signal (not SIGKILL,SIGSTOP), 
+                           //handler = SIG_DFL - back to default behavior 
+  
+  pid_t child=fork();
+  if (child>0)
+  { 
+    pause(); //waits till a signal arrive 
+    printf("Signal arrived\n",SIGRTMIN);
+    int status;
+    wait(&status);
+    printf("Parent process ended\n");
+  }
+  else 
+  {
+    printf("Waits 3 seconds, then send a SIGTERM %i signal\n",SIGRTMIN);
+    sleep(3);
+    kill(getppid(),SIGRTMIN);
+    kill(getppid(),SIGRTMIN); 
+    //1. parameter the pid number of process, we send the signal
+    // 		if -1, then eacho of the processes of the same uid get the signal
+    // 		we kill our bash as well! The connection will close
+    //2. parameter the name or number of signal
+    printf("Child process ended\n");  
+  }
+  return 0;
+}
