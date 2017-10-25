387e5f1a8363608ad0073e0b9dbbaec4b82d4f5f
Sandor Szaloki <ishatter830@gmail.com>
2017-10-25 21:55:05 +0200

    First try

--- orsi/bead1/main.cpp
+++ orsi/bead1/main.cpp
@@ -2,10 +2,22 @@
 #include <vector>
 #include <future>
 #include <iostream>
+#include <cstring>
+#include <numeric>
+
+static const uint64_t CODE = 0x666;
+static const uint64_t FF = 0xFF;
+static const uint64_t MASK = 0x12345678;
 
 std::vector<std::string> readLines(std::string filename);
 std::vector<std::future<std::string>> hashLines(std::vector<std::string> lines);
-void process(std::vector<std::future<std::string>> hashedLines);
+std::vector<std::string> process(std::vector<std::future<std::string>> hashedLines);
+void printResult(std::vector<std::string> result);
+
+bool isPrime(const uint64_t& n);
+uint64_t hash(const char& letter);
+std::string hashWord(std::string word);
+std::string hashLine(std::string line);
 
 int main (int argc, char** argv) {
     if (argc != 2) {
@@ -14,7 +26,8 @@ int main (int argc, char** argv) {
     }
     std::vector<std::string> lines(readLines(argv[1]));
     std::vector<std::future<std::string>> hashedData(hashLines(lines));
-    process(hashedData);
+    std::vector<std::string> result(process(hashedData));
+    printResult(result);
     return 0;
 }
 
@@ -35,10 +48,58 @@ std::vector<std::string> readLines(std::string filename) {
 }
 
 std::vector<std::future<std::string>> hashLines(std::vector<std::string> lines) {
-    std::vector<std::future<std::string>> out;
-    return out;
+    return 
+        std::transform(
+            lines.begin(),
+            lines.end(),
+            lines.being(),
+            [](auto& line) {return std::async(std::launch::async, hashLine, line);}
+        );
 }
 
-void process(std::vector<std::future<std::string>> hashedLines) {
+std::vector<std::string> process(std::vector<std::future<std::string>> hashedLines) {
+    return 
+        std::transform(
+            hashedLines.begin(),
+            hashedLines.end(),
+            hashedLines.being(),
+            [](auto& line) {return line.get();}
+        );
+}
 
+void printResult(std::vector<std::string> result) {
+    std::ofstream file("output.txt");
+    for(auto& line : result) {
+        ofstream << result << " " << std::endl;
+    }
+}
+
+bool isPrime(const uint64_t& n) {
+    (n<=1)?return false:(n<=3)?return true:(n%2==0||n%3==0)?return false:int i=5, while(i*i < n){if(n%i==0||n%(i+2)==0)return false;i=i+6;}, return true;
+}
+
+uint64_t hash(const char& letter) {
+    uint64_t value = CODE <<= (letter % 2 == 0)?6:11 ^= FF;
+    return isPrime(value)?value|MASK:value&MASK;
+}
+
+std::string hashWord(std::string word) {
+    return
+        std::accumulate(
+            word,
+            word + word.length(),
+            0,
+            [](uint64_t state, const char& value) {return state + hash(value)}
+        );
+}
+
+std::string hashLine(std::string line) {
+    std::vector<string> words(std::strtok(line, " "));
+    return
+        std::accumulate(
+            words.begin(),
+            words.end(),
+            "",
+            [](std::string& state, std::string& value){return state + " " + hashWord(value);}
+        );
 }
