e8c514deca6c9a56b14b9abcdf289a1102253814
Sandor Szaloki <ishatter830@gmail.com>
2017-10-25 22:15:58 +0200

    Fix transforms. Add const refs

--- orsi/bead1/main.cpp
+++ orsi/bead1/main.cpp
@@ -10,15 +10,15 @@ static const uint64_t CODE = 0x666;
 static const uint64_t FF = 0xFF;
 static const uint64_t MASK = 0x12345678;
 
-std::vector<std::string> readLines(std::string filename);
-std::vector<std::future<std::string>> hashLines(std::vector<std::string> lines);
-std::vector<std::string> process(std::vector<std::future<std::string>> hashedLines);
-void printResult(std::vector<std::string> result);
+std::vector<std::string> readLines(const std::string& filename);
+std::vector<std::future<std::string>> hashLines(const std::vector<std::string>& lines);
+std::vector<std::string> process(const std::vector<std::future<std::string>>& hashedLines);
+void printResult(const std::vector<std::string>& result);
 
 bool isPrime(const uint64_t& n);
 uint64_t hash(const char& letter);
-std::string hashWord(std::string word);
-std::string hashLine(std::string line);
+std::string hashWord(const std::string& word);
+std::string hashLine(const std::string& line);
 
 int main (int argc, char** argv) {
     if (argc != 2) {
@@ -32,7 +32,7 @@ int main (int argc, char** argv) {
     return 0;
 }
 
-std::vector<std::string> readLines(std::string filename) {
+std::vector<std::string> readLines(const std::string& filename) {
     std::vector<std::string> out;
     std::ifstream file(filename);
     int size;
@@ -48,27 +48,29 @@ std::vector<std::string> readLines(std::string filename) {
     return out;
 }
 
-std::vector<std::future<std::string>> hashLines(std::vector<std::string> lines) {
-    return 
-        std::transform(
-            lines.begin(),
-            lines.end(),
-            lines.begin(),
-            [](std::string& line) {return std::async(std::launch::async, hashLine, line);}
-        );
+std::vector<std::future<std::string>> hashLines(const std::vector<std::string>& lines) {
+    std::vector<std::future<std::string>> result(lines.size());
+    std::transform(
+        lines.begin(),
+        lines.end(),
+        result.begin(),
+        [](const std::string& line) {return std::async(std::launch::async, hashLine, line);}
+    );
+    return result;
 }
 
-std::vector<std::string> process(std::vector<std::future<std::string>> hashedLines) {
-    return 
-        std::transform(
-            hashedLines.begin(),
-            hashedLines.end(),
-            hashedLines.begin(),
-            [](std::future<std::string>>& line) {return line.get();}
-        );
+std::vector<std::string> process(const std::vector<std::future<std::string>>& hashedLines) {
+    std::vector<std::string> result(hashLines.size());
+    std::transform(
+        hashedLines.begin(),
+        hashedLines.end(),
+        result.begin(),
+        [](std::future<std::string>>& line) {return line.get();}
+    );
+    return result;
 }
 
-void printResult(std::vector<std::string> result) {
+void printResult(const std::vector<std::string>& result) {
     std::ofstream file("output.txt");
     for(auto& line : result) {
         ofstream << result << " " << std::endl;
@@ -76,7 +78,19 @@ void printResult(std::vector<std::string> result) {
 }
 
 bool isPrime(const uint64_t& n) {
-    (n<=1)?return false:(n<=3)?return true:(n%2==0||n%3==0)?return false:int i=5, while(i*i < n){if(n%i==0||n%(i+2)==0)return false;i=i+6;}, return true;
+    if (n<=1) {
+        return false;
+    else if (n<=3) {
+        return true;
+    else if (n%2==0||n%3==0) {
+        return false;
+    int i=5;
+    while(i*i < n){
+        if (n%i==0||n%(i+2)==0)
+            return false;
+        i=i+6;
+    }
+    return true;
 }
 
 uint64_t hash(const char& letter) {
@@ -84,7 +98,7 @@ uint64_t hash(const char& letter) {
     return isPrime(value)?value|MASK:value&MASK;
 }
 
-std::string hashWord(std::string word) {
+std::string hashWord(const std::string& word) {
     return
         std::accumulate(
             word,
@@ -94,13 +108,13 @@ std::string hashWord(std::string word) {
         );
 }
 
-std::string hashLine(std::string line) {
+std::string hashLine(const std::string& line) {
     std::vector<string> words(std::strtok(line, " "));
     return
         std::accumulate(
             words.begin(),
             words.end(),
             "",
-            [](std::string& state, std::string& value){return state + " " + hashWord(value);}
+            [](std::string& state, const std::string& value){return state + " " + hashWord(value);}
         );
 }
