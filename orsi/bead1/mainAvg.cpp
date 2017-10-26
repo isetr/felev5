c215122cefe2ea7efdffd28a28ee3874e408f0d4
Sandor Szaloki <ishatter830@gmail.com>
2017-10-26 23:21:04 +0200

    Add a version to get average calculation time

---
+++ orsi/bead1/mainAvg.cpp
@@ -0,0 +1,139 @@
+#include <fstream>
+#include <vector>
+#include <future>
+#include <iostream>
+#include <string>
+#include <numeric>
+#include <algorithm>
+#include <sstream>
+#include <chrono>
+
+static const uint32_t CODE = 0x666;
+static const uint32_t FF = 0xFF;
+static const uint32_t MASK = 0x12345678;
+
+const std::vector<std::string> readLines(int argc, char** argv);
+std::vector<std::future<std::string>> hashLines(std::vector<std::string>& lines);
+const std::vector<std::string> process(std::vector<std::future<std::string>>& hashedLines);
+void printResult(const std::vector<std::string>& result);
+
+bool isPrime(uint32_t n);
+uint32_t hash(char letter);
+uint32_t hashWord(const std::string& word);
+std::string hashLine(const std::string& line);
+
+int main (int argc, char** argv) {
+    std::vector<std::string> lines(readLines(argc, argv));
+    double t = 0;
+
+    for(int i = 0; i < 10; ++i) {
+        auto start = std::chrono::steady_clock::now();
+
+        std::vector<std::future<std::string>> hashedData(hashLines(lines));
+        std::vector<std::string> result(process(hashedData));
+
+        auto end = std::chrono::steady_clock::now();
+        t += std::chrono::duration<double>(end - start).count();
+        printResult(result);
+    }
+    std::cerr << "Time elapsed: " << t / 10 << " sec" << std::endl;
+
+    return 0;
+}
+
+const std::vector<std::string> readLines(int argc, char** argv) {
+    std::vector<std::string> out;
+    std::ifstream file;
+    int size;
+    
+    if (argc == 2) {
+        file.open(argv[1]);
+    } else {
+        file.open("input.txt");
+    }
+
+    file >> size;
+    
+    out.reserve(size);
+    
+    std::string line;
+    std::getline(file, line); // first empty line after the number
+    while(std::getline(file, line)) {
+        out.push_back(line);
+    }
+
+    return out;
+}
+
+std::vector<std::future<std::string>> hashLines(std::vector<std::string>& lines) {
+    std::vector<std::future<std::string>> result(lines.size());
+    std::transform(
+        lines.begin(),
+        lines.end(),
+        result.begin(),
+        [](std::string& line) {return std::async(std::launch::async, hashLine, line);}
+    );
+    return result;
+}
+
+const std::vector<std::string> process(std::vector<std::future<std::string>>& hashedLines) {
+    std::vector<std::string> result(hashedLines.size());
+    std::transform(
+        hashedLines.begin(),
+        hashedLines.end(),
+        result.begin(),
+        [](std::future<std::string>& line) {return line.get();}
+    );
+    return result;
+}
+
+void printResult(const std::vector<std::string>& result) {
+    std::ofstream file("output.txt");
+    for(auto& line : result) {
+        file << line << std::endl;
+    }
+}
+
+bool isPrime(uint32_t n) {
+    if (n<=1) {
+        return false;
+    } else if (n<=3) {
+        return true;
+    } else if (n%2==0||n%3==0) {
+        return false;
+    }
+    int i=5;
+    while(i*i < n) {
+        if (n%i==0||n%(i+2)==0)
+            return false;
+        i=i+6;
+    }
+    return true;
+}
+
+uint32_t hash(char letter) {
+    uint32_t value = CODE;
+    value <<= (letter % 2 == 0)?6:11;
+    value ^= letter & FF;
+    return isPrime(value)?value|MASK:value&MASK;
+}
+
+uint32_t hashWord(const std::string& word) {
+    return
+        std::accumulate(
+            word.begin(),
+            word.end(),
+            0,
+            [](uint32_t state, char value) {return state + hash(value);}
+        );
+}
+
+std::string hashLine(const std::string& line) {
+    std::stringstream words(line);
+    std::stringstream output;
+    std::string word;
+    while(words >> word) {
+        output << hashWord(word) << " ";
+    }
+    return output.str();
+}
