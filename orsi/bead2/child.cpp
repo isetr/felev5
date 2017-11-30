371fa3d2753d049a82a245c4630235c66e2b8560
Sandor Szaloki <ishatter830@gmail.com>
2017-11-30 23:22:58 +0100

    Orsi | Bead 2 - Working?

--- orsi/bead2/child.cpp
+++ orsi/bead2/child.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <cstdlib>
 
 #include "pvm3.h"
 
@@ -7,25 +8,48 @@ int main(int argc, char** argv) {
     int ptid = pvm_parent();
     int sum;
     int setsize;
-    std::vector<int> set;
+    int* set;
+    int result;
     
     pvm_recv(ptid, -1);
     pvm_upkint(&setsize, 1, 1);
-    for(int i = 0; i < setsize; ++i) {
-        int tmp;
-        pvm_upkint(&tmp, 1, 1);
-        set.push_back(tmp);
-    }
-    pcm_upkint(&sum, 1, 1);
-
-    int result;
+    pvm_upkint(set, setsize, 1);
+    pvm_upkint(&sum, 1, 1);
+    pvm_upkint(&result, 1, 1);
 
     if (sum == 0) {
-        result = 0;
+        result *= 0;
     } else if(setsize == 0) {
-        result = 1;
+        result *= 1;
     } else {
+        int r1, r2;    
+        int tid1, tid2;
+        int ss = setsize - 1;
+        int sum1 = sum - set[setsize - 1];
         
+        if(pvm_spawn(const_cast<char*>("child"), (char**)0, 0, const_cast<char*>(""), 1, &tid1) < 1) std::exit(-1);
+        pvm_initsend(PvmDataDefault);
+        pvm_pkint(&ss, 1, 1);
+        pvm_pkint(set, setsize - 1, 1);
+        pvm_pkint(&sum1, 1, 1);
+        pvm_pkint(0, 1, 1);
+        pvm_send(tid1, 0);
+
+        if(pvm_spawn(const_cast<char*>("child"), (char**)0, 0, const_cast<char*>(""), 1, &tid2) < 1) std::exit(-1);
+        pvm_initsend(PvmDataDefault);
+        pvm_pkint(&ss, 1, 1);
+        pvm_pkint(set, setsize - 1, 1);
+        pvm_pkint(&sum, 1, 1);
+        pvm_pkint(0, 1, 1);
+        pvm_send(tid2, 0);
+
+        pvm_recv(tid1, -1);
+        pvm_upkint(&r1, 1, 1);
+        
+        pvm_recv(tid2, -1);
+        pvm_upkint(&r2, 1, 1);
+        
+        result *= r1 * r2;
     }
 
     pvm_initsend(PvmDataDefault);
--- orsi/bead2/master.cpp
+++ orsi/bead2/master.cpp
@@ -5,25 +5,27 @@
 
 #include "pvm3.h"
 
-std::vector<int> readFile(std::string path);
-void writeFile(std::string path, bool result);
+int* readFile(std::string path, int& count);
+void writeFile(std::string path, int result);
 
 int main(int argc, char** argv) {
     int sum = atoi(argv[1]);
-    std::vector<int> set = readFile(argv[2]);
+    int setsize;
+    int* set = readFile(argv[2], setsize);
+    int result = 1;
     
     int tid;
-    pvm_spawn(const_cast<char*>("child"), (char**)0, 0, const_cast<char*>(""), 1, tid);
+    if(pvm_spawn(const_cast<char*>("child"), (char**)0, 0, const_cast<char*>(""), 1, &tid) < 1) return -1;
     
     pvm_initsend(PvmDataDefault);
-    pvm_pkint(set.size(), 1, 1);
-    pvm_pkint(&set[0], set.size(), 1);
-    pvm_pkint(sum, 1, 1);
+    pvm_pkint(&setsize, 1, 1);
+    pvm_pkint(set, setsize, 1);
+    pvm_pkint(&sum, 1, 1);
+    pvm_pkint(&result, 1, 1);
     pvm_send(tid, 0);
 
     pvm_recv(tid, -1);
 
-    int result;
     pvm_upkint(&result, 1, 1);
 
     writeFile(argv[3], result);
@@ -32,28 +34,25 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-std::vector<int> readFile(std::string path) {
+int* readFile(std::string path, int& count) {
     std::ifstream file(path);
-
-    int count;
     file >> count;
-
-    std::vector<int> out;
-    out.reserve(count);
-
-    int tmp;
+    int* out = new int[count];
     for(int i = 0; i < count; ++i) {
-        file >> tmp;
-        out.push_back(tmp);
+        file >> out[i];
     }
+
+    return out;
 }
 
-void writeFile(std::string path, bool result) {
+void writeFile(std::string path, int result) {
     std::ofstream file(path);
 
-    if(result) {
-        file.write("May the subset be with you!");
+    if(!result) {
+        file << "May the subset be with You!";
     } else {
-        file.write("I find your lack of subset disturbing!");
+        file << "I find your lack of subset disturbing!";
     }
+
+    file.close();
 }
